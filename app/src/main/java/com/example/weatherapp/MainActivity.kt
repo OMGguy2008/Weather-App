c36de655d5bf631d7bab6a4ab8470dfc41214b1f
OMGguy2008 <omggamer2008@gmail.com>
2025-02-11 22:03:27 +0200

    Trying to extract all the json data to a class, crashes

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.weatherapp
 
 import android.os.Bundle
-import android.util.Log;
-import android.widget.Button
-import android.widget.TextView
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,17 +14,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.get
-import io.ktor.client.request.request
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
-import io.ktor.http.HttpMethod
-import io.ktor.http.URLProtocol
-import io.ktor.http.parseClientCookiesHeader
-import io.ktor.http.path
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonObject
 
+//Data classes
+@Serializable
+data class currentWeatherForecast(
+    val last_updated: String
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,20 +44,17 @@ class MainActivity : ComponentActivity() {
                     )
                 }
             }
-
-            //Button
-            val btn: Button = findViewById<Button>(R.id.searchbutton)
-            btn.setOnClickListener{
                 runBlocking {
                     launch {
-                        setContentView(R.layout.activity_main)
-
-                        val response: HttpResponse? = getData()
-                        val text = findViewById<TextView>(R.id.apioutput)
+                        val response: HttpResponse? = getData("London")
                         if (response != null) {
-                            text.text = response.bodyAsText()
+                            Log.i("response", response.bodyAsText())
+                            //Parsing the Data
+                            val data = Json{ignoreUnknownKeys = true}.decodeFromString<currentWeatherForecast>(response.body())
+                            Log.i("Data", "Data from: " + data.last_updated)
                         } else{
-                            text.text = "ERROR"
+                            Log.e("Error", "Failed to request data")
+                            return@launch
                         }
                     }
                 }
@@ -62,30 +62,30 @@ class MainActivity : ComponentActivity() {
 
         }
     }
-}
-
 
 
 //Data
-suspend fun getData(): HttpResponse? {
+suspend fun getData(city: String): HttpResponse? {
+    val client = HttpClient()
     try {
         //Variables
         val url = "http://api.weatherapi.com/v1/current.json"
         val key = "95e088be19344374a8d174802242903"
         //Networking code
-        val client = HttpClient()
+
         val response: HttpResponse = client.get(url) {
             url {
                 parameters.append("key", key)
-                parameters.append("q", "Kaunas")
+                parameters.append("q", city)
             }
         }
         Log.i("Data", response.status.toString())
-        client.close()
         return response
     } catch(e: Exception){
         e.printStackTrace()
         return null
+    } finally {
+        client.close()
     }
 }
 
