0931cb8b449d134c8d02f9b9d40685640c7aa700
OMGguy2008 <omggamer2008@gmail.com>
2025-02-26 19:32:47 +0200

    Added thye deserialisation of the json string received from the current realtime weather data api endpoint

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -26,10 +26,72 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.jsonObject
 
 //Data classes
+//Helper data classes
+//Location data class
 @Serializable
-data class currentWeatherForecast(
-    val last_updated: String
+data class Location(
+    val name: String,
+    val region: String,
+    val country: String,
+    val lat: Double,
+    val lon: Double,
+    val tz_id: String,
+    val localtime_epoch: Int,
+    val localtime: String
 )
+
+//Weather data class
+@Serializable
+data class CurrentWeather(
+    val last_updated_epoch: Long,
+    val last_updated: String,
+    val temp_c: Double,
+    val temp_f: Double,
+    val is_day: Int,
+    val condition: Condition,
+    val wind_mph: Double,
+    val wind_kph: Double,
+    val wind_degree: Int,
+    val wind_dir: String,
+    val pressure_mb: Double,
+    val pressure_in: Double,
+    val precip_mm: Double,
+    val precip_in: Double,
+    val humidity: Int,
+    val cloud: Int,
+    val feelslike_c: Double,
+    val feelslike_f: Double,
+    val windchill_c: Double,
+    val windchill_f: Double,
+    val heatindex_c: Double,
+    val heatindex_f: Double,
+    val dewpoint_c: Double,
+    val dewpoint_f: Double,
+    val vis_km: Double,
+    val vis_miles: Double,
+    val uv: Double,
+    val gust_mph: Double,
+    val gust_kph: Double
+)
+
+//Condition decoration data thing
+@Serializable
+data class Condition(
+    val text: String,
+    val icon: String,
+    val code: Int
+)
+
+
+//Current Weather Class
+@Serializable
+data class weatherData(
+    val location: Location,
+    val current: CurrentWeather
+)
+
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,8 +112,8 @@ class MainActivity : ComponentActivity() {
                         if (response != null) {
                             Log.i("response", response.bodyAsText())
                             //Parsing the Data
-                            val data = Json{ignoreUnknownKeys = true}.decodeFromString<currentWeatherForecast>(response.body())
-                            Log.i("Data", "Data from: " + data.last_updated)
+                            val data = Json.decodeFromString<weatherData>(response.bodyAsText())
+                            Log.i("Data output", "Dabartinė temperatūra: " + data.current.feelslike_c.toString() + " C")
                         } else{
                             Log.e("Error", "Failed to request data")
                             return@launch
